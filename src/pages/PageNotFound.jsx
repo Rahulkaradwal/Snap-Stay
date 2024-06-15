2a062b75072fc82cff9958ee17d04e8214e706a1
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-06-15 15:04:34 -0400

    feat: add dependencies

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import { useMoveBack } from "../hooks/useMoveBack";
-import Heading from "../ui/Heading";
+import { useMoveBack } from '../hooks/useMoveBack';
+import Heading from '../ui/Heading';
 
 const StyledPageNotFound = styled.main`
   height: 100vh;
